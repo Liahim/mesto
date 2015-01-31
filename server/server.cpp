1a22a41eb889e1fe9bfc458a300cb2266f35379f
Mihail Marinov <m.marinov@samsung.com>
2015-01-30 16:16:27 -0800

    use streams support multiple devices

--- server/server.cpp
+++ server/server.cpp
@@ -18,137 +18,165 @@
 #include <time.h>
 #include <signal.h>
 #include <pthread.h>
-#include <string>
 
-static const char* SYSLOG_TAG="Mesto";
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <iostream>
+
+static const char* SYSLOG_TAG = "Mesto";
 
 void* doprocessing(void* arg);
 
+pthread_mutex_t lock;
+
+/**
+ * port parameter
+ * output file location/dir
+ */
 int main(int argc, char *argv[]) {
-	openlog(SYSLOG_TAG,LOG_PERROR,0);
-	syslog(LOG_INFO, "starting up");
+    openlog(SYSLOG_TAG, LOG_PERROR, 0);
+    syslog(LOG_INFO, "starting up");
 
-	struct sigaction sigchld_action;
-	sigchld_action.sa_handler = SIG_DFL;
-	sigchld_action.sa_flags = SA_NOCLDWAIT;
-	sigaction(SIGCHLD, &sigchld_action, NULL);
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        syslog(LOG_ERR, "ERROR mutex init failed");
+        return 1;
+    }
 
-	int sockfd, newsockfd, portno;
-	socklen_t clilen;
-	struct sockaddr_in serv_addr, cli_addr;
+    struct sigaction sigchld_action;
+    sigchld_action.sa_handler = SIG_DFL;
+    sigchld_action.sa_flags = SA_NOCLDWAIT;
+    sigaction(SIGCHLD, &sigchld_action, NULL);
 
-	/* First call to socket() function */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		syslog(LOG_ERR, "ERROR opening socket");
-		exit(1);
-	}
-	/* Initialize socket structure */
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = 5001;
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
 
-	/* Now bind the host address using bind() call.*/
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		syslog(LOG_ERR, "ERROR on binding");
-		exit(1);
-	}
-	/* Now start listening for the clients, here
-	 * process will go in sleep mode and will wait
-	 * for the incoming connection
-	 */
-	listen(sockfd, 5);
-	clilen = sizeof(cli_addr);
-	while (1) {
-		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    /* First call to socket() function */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        syslog(LOG_ERR, "ERROR opening socket");
+        exit(1);
+    }
+    /* Initialize socket structure */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = 5001;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
 
-		if (newsockfd < 0) {
-			syslog(LOG_ERR, "ERROR on accept");
-			exit(1);
-		}
-		/* Create child process */
-		pthread_t thread;
-		int err = pthread_create(&thread, NULL, doprocessing, (void*)&newsockfd);
-		if (0 != err) {
-	        syslog(LOG_ERR, "error creating thread; err: %X", err);
-	        exit(1);
-		}
-	} /* end of while */
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        syslog(LOG_ERR, "ERROR on binding");
+        exit(1);
+    }
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-	closelog();
+        if (newsockfd < 0) {
+            syslog(LOG_ERR, "ERROR on accept");
+            exit(1);
+        }
+
+        pthread_t thread;
+        int err = pthread_create(&thread, NULL, doprocessing, (void*) &newsockfd);
+        if (0 != err) {
+            syslog(LOG_ERR, "error creating thread; err: %X", err);
+            exit(1);
+        }
+    }
+
+    pthread_mutex_destroy(&lock);
+    closelog();
 }
 
+std::map<std::string, std::string> gMap;
+
 void* doprocessing(void* arg) {
-	int n;
-	char buffer[256];
+    int n;
+    char buffer[256];
 
-	int sock = *((int*)arg);
+    int sock = *((int*) arg);
 
-	bzero(buffer, 256);
+    bzero(buffer, 256);
 
-	n = read(sock, buffer, 255);
-	if (n < 16) {
-		syslog(LOG_ERR, "ERROR reading from socket");
-		exit(1);
-	}
+    n = read(sock, buffer, 255);
+    if (n < 16) {
+        syslog(LOG_ERR, "ERROR reading from socket");
+        exit(1);
+    }
 
-	union myunion {
-		char b[8];
-		double d;
-	} lat, lon;
+    union myunion {
+        char b[8];
+        double d;
+    } lat, lon;
 
-	int offset = 0;
+    int offset = 0;
 
-	//udn utf-8 string
-	short udnLength = buffer[offset++]<<8;
-	udnLength |= buffer[offset++];
+    //udn utf-8 string
+    short udnLength = buffer[offset++] << 8;
+    udnLength |= buffer[offset++];
 
-	std::string id(buffer+offset, udnLength);
-	offset += udnLength;
+    std::string id(buffer + offset, udnLength);
+    offset += udnLength;
 
-	//title utf-8 string
-	short titleLength = buffer[offset++]<<8;
-	titleLength |= buffer[offset++];
+    //title utf-8 string
+    short titleLength = buffer[offset++] << 8;
+    titleLength |= buffer[offset++];
 
-    std::string title(buffer+offset, titleLength);
+    std::string title(buffer + offset, titleLength);
     offset += titleLength;
 
-	lon.b[7] = buffer[offset++];
-	lon.b[6] = buffer[offset++];
-	lon.b[5] = buffer[offset++];
-	lon.b[4] = buffer[offset++];
-	lon.b[3] = buffer[offset++];
-	lon.b[2] = buffer[offset++];
-	lon.b[1] = buffer[offset++];
-	lon.b[0] = buffer[offset++];
+    lon.b[7] = buffer[offset++];
+    lon.b[6] = buffer[offset++];
+    lon.b[5] = buffer[offset++];
+    lon.b[4] = buffer[offset++];
+    lon.b[3] = buffer[offset++];
+    lon.b[2] = buffer[offset++];
+    lon.b[1] = buffer[offset++];
+    lon.b[0] = buffer[offset++];
 
-	lat.b[7] = buffer[offset++];
-	lat.b[6] = buffer[offset++];
-	lat.b[5] = buffer[offset++];
-	lat.b[4] = buffer[offset++];
-	lat.b[3] = buffer[offset++];
-	lat.b[2] = buffer[offset++];
-	lat.b[1] = buffer[offset++];
-	lat.b[0] = buffer[offset++];
+    lat.b[7] = buffer[offset++];
+    lat.b[6] = buffer[offset++];
+    lat.b[5] = buffer[offset++];
+    lat.b[4] = buffer[offset++];
+    lat.b[3] = buffer[offset++];
+    lat.b[2] = buffer[offset++];
+    lat.b[1] = buffer[offset++];
+    lat.b[0] = buffer[offset++];
 
-	int fd = open ("/opt/share/httpd/coord.json", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	char msg[128] = {0};
-	sprintf(msg, "{\"lon\":%.6lf,\"lat\":%.6lf,\"title\":\"%s\"}", lon.d, lat.d, title.c_str());
-	write(fd, msg, strlen(msg));
-	close(fd);
+    std::stringstream ss;
+    ss << "{\"lon\":" << lon.d << ",\"lat\":" << lat.d << ",\"title\":\"" << title << "\"}";
+    std::string sss = ss.str();
 
-	time_t t = time(NULL);
-	struct tm tm = *localtime(&t);
-	printf("%d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
-			tm.tm_hour, tm.tm_min, tm.tm_sec);
+    pthread_mutex_lock(&lock);
+    gMap[title] = sss;
+    pthread_mutex_unlock(&lock);
 
-	syslog(LOG_INFO, "Here is the message: %f, %f",lat.d, lon.d);
+    std::ofstream file;
+    file.open("/opt/share/httpd/coord.json", std::ios::out | std::ios::trunc);
+    if (file.is_open()) {
+        file << "\"devices\":[" << std::endl;
+        for (auto& item : gMap) {
+            std::cout << item.first << "   " << item.second << std::endl;
+            file << item.second << ',' << std::endl;
+        }
+        file << "]" << std::endl;
+        file.close();
+    } else {
+        syslog(LOG_ERR, "cannot open output file");
+        exit(1);
+    }
 
-	n = write(sock, "I got your message", 18);
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf("%d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    syslog(LOG_INFO, "Here is the message: %f, %f", lat.d, lon.d);
 
-	close(sock);
-	return NULL;
+    n = write(sock, "gotit", 5);
+    close(sock);
+    return nullptr;
 }
 
